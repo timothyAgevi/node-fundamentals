08fd47795ee981cbcf22aafef32d833356bb57d5
timothyAgevi <timothyagevi@gmail.com>
2022-06-22 18:48:38 +0300

    update on output.txt

--- NODE-FARM/index.js
+++ NODE-FARM/index.js
@@ -1,5 +1,9 @@
 const fs =require('fs');
 
  const textIn= fs.readFileSync('./txt/input.txt','utf-8');
-
  console.log(textIn);
+
+ const textOut ='This is how to write into files:${textIn}.\n Creted on ${Date.now()}';
+ fs.writeFileSync('./txt/output.txt',textOut);
+ console.log('Filewriten!');
+ 
